a020b71b5dbfd7f500ce008d20a65486bdcac02e
MuhammadMahbub <muhammadmahbub07@gmail.com>
2023-07-01 08:58:55 +0600

    home page 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,6 +44,31 @@ $(document).ready(function() {
         infinite: true,
         slidesToShow: 6,
         slidesToScroll: 1,
+        pauseOnHover : true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 6,
+                    slidesToScroll: 6,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 4,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            }
+    
+      ]
     });
 
 
